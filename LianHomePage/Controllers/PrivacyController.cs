cd890600a6c6a182d58ee09f1a7de82c1bf30883
quanminhvo <quanminhvo@gmail.com>
2023-11-30 14:02:05 +0700

    update from old source

---
+++ LianHomePage/Controllers/PrivacyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LianHomePage.Controllers
+{
+    [Route("bao-mat-va-quyen-rieng-tu")]
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
